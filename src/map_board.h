ae6ab1688a63ac65f2e289131a491930c55e242e
usermicrodevices <maxkolosov@inbox.ru>
2024-02-25 20:58:30 +0300

    Update map_board.h

--- src/map_board.h
+++ src/map_board.h
@@ -18,8 +18,8 @@ class MapBoardCtrl : public wxScrolledWindow
 
 		void OnPaint(wxPaintEvent& WXUNUSED(evt))
 		{
-			wxString s;
-			int h, w, height;
+			//wxString s;
+			//int h, w, height;
 
 			size_t mapInPx = FromDIP(MapInDIPs);
 			size_t cellSideInPx = FromDIP(CellSideInDIPs);
@@ -27,18 +27,21 @@ class MapBoardCtrl : public wxScrolledWindow
 			wxPaintDC dc(this);
 			wxSize size = GetClientSize();
 
+			size_t x_cell_selected = (current_mouse_position.x < (int)cellSideInPx) ? 0 : abs(current_mouse_position.x / (int)cellSideInPx) * (int)cellSideInPx;
+			size_t y_cell_selected = (current_mouse_position.y < (int)cellSideInPx) ? 0 : abs(current_mouse_position.y / (int)cellSideInPx) * (int)cellSideInPx;
+
 			wxRect cellSelected(wxSize(cellSideInPx, cellSideInPx));
 			cellSelected.SetLeft(0);
-			cellSelected.Offset(current_mouse_position);
+			cellSelected.Offset(x_cell_selected, y_cell_selected);
 			dc.DrawRectangle(cellSelected);
 
 			dc.SetBrush(wxBrush(wxColour(200, 0, 0, 50)));
 			dc.SetPen(wxPen(wxColour(0, 0, 255, 128), 10));
 			dc.DrawRectangle(0, 0, size.x, size.y);
 
-			dc.SetPen(wxPen(wxColour(0, 200, 0, 128), 5));
-			dc.DrawLine(0, 0, size.x, size.y);
-			dc.DrawLine(0, size.y, size.x, 0);
+			//dc.SetPen(wxPen(wxColour(0, 200, 0, 128), 5));
+			//dc.DrawLine(0, 0, size.x, size.y);
+			//dc.DrawLine(0, size.y, size.x, 0);
 
 			dc.SetPen(wxPen(wxColour(0, 255, 0, 128), 10));
 			for(size_t xpos = cellSideInPx; xpos < mapInPx; xpos+=CellSideInDIPs)
@@ -46,33 +49,33 @@ class MapBoardCtrl : public wxScrolledWindow
 			for(size_t ypos = cellSideInPx; ypos < mapInPx; ypos+=CellSideInDIPs)
 				dc.DrawLine(0, ypos, mapInPx, ypos);
 
-			s.Printf("Size: %d x %d", size.x, size.y);
+			//s.Printf("Size: %d x %d", size.x, size.y);
 
-			dc.SetFont(*wxNORMAL_FONT);
-			dc.GetTextExtent(s, &w, &height);
-			height += FromDIP(3);
-			dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2));
+			//dc.SetFont(*wxNORMAL_FONT);
+			//dc.GetTextExtent(s, &w, &height);
+			//height += FromDIP(3);
+			//dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2));
 
-			if (m_mgr)
-			{
-				wxAuiPaneInfo pi = m_mgr->GetPane(this);
+			//if (m_mgr)
+			//{
+				//wxAuiPaneInfo pi = m_mgr->GetPane(this);
 
-				s.Printf("Layer: %d", pi.dock_layer);
-				dc.GetTextExtent(s, &w, &h);
-				dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*1));
+				//s.Printf("Layer: %d", pi.dock_layer);
+				//dc.GetTextExtent(s, &w, &h);
+				//dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*1));
 
-				s.Printf("Dock: %d Row: %d", pi.dock_direction, pi.dock_row);
-				dc.GetTextExtent(s, &w, &h);
-				dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*2));
+				//s.Printf("Dock: %d Row: %d", pi.dock_direction, pi.dock_row);
+				//dc.GetTextExtent(s, &w, &h);
+				//dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*2));
 
-				s.Printf("Position: %d", pi.dock_pos);
-				dc.GetTextExtent(s, &w, &h);
-				dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*3));
+				//s.Printf("Position: %d", pi.dock_pos);
+				//dc.GetTextExtent(s, &w, &h);
+				//dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*3));
 
-				s.Printf("Proportion: %d", pi.dock_proportion);
-				dc.GetTextExtent(s, &w, &h);
-				dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*4));
-			}
+				//s.Printf("Proportion: %d", pi.dock_proportion);
+				//dc.GetTextExtent(s, &w, &h);
+				//dc.DrawText(s, (size.x-w)/2, ((size.y-(height*5))/2)+(height*4));
+			//}
 		}
 
 		void OnEraseBackground(wxEraseEvent& WXUNUSED(evt))
@@ -91,7 +94,8 @@ class MapBoardCtrl : public wxScrolledWindow
 			wxString s;
 			s.Printf("Position: (%d,%d)", current_mouse_position.x, current_mouse_position.y);
 			((wxFrame*)m_mgr->GetManagedWindow())->SetStatusText(s);
-		}	
+			Refresh();
+		}
 
 		wxDECLARE_EVENT_TABLE();
 };
